0bbad405b817fe06f932c1a2f0bbaa7c38d62467
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-24 10:43:27 -0400

    .

---
+++ day4/script.js
@@ -0,0 +1,22 @@
+//alert("testing my js file")
+
+
+//document.getElementById("mymessage").innerHTML = "Welcome to learning Javascript"
+
+
+document.getElementById("mydate").innerHTML = Date()
+
+/*
+function nameyourfunction()
+{
+
+}
+*/
+function MyGreetingMsg()
+{
+
+    document.getElementById("mymessage").innerHTML = "Welcome to learning CSS"
+
+
+}
+
