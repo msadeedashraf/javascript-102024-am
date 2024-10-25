4f6acb290b255ea6da4db8fb4f566c82c8d27117
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-25 11:03:51 -0400

    .

---
+++ day5/script.js
@@ -0,0 +1,11 @@
+function increaseFont()
+{
+    document.getElementById('demo').style.fontSize='35px'
+}
+
+
+function decreaseFont()
+{
+
+    document.getElementById('demo').style.fontSize='16px'
+}
