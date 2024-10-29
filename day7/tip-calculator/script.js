23ba5e8538ddbf7e59bc6505fe3f1dcc858a33ff
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-10-29 10:43:50 -0400

    .

--- day7/tip-calculator/script.js
+++ day7/tip-calculator/script.js
@@ -1,1 +1,14 @@
 //alert("Hello")
+
+
+//var myBill =  document.getElementById("bill");
+//console.log(myBill);
+
+function calculatetip()
+{
+    var myBillAmount =  document.getElementById("bill").value;
+    var myTip =  document.getElementById("tip").value;
+    var totalAmount = myBillAmount * (1+myTip/100);
+    document.getElementById('total').innerHTML = totalAmount.toFixed(2);
+
+}
