8947eea29f37fa8df4da3dcfb77cd3612a977a6e
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-11-05 11:21:10 -0500

    .

--- day12/BankAccount/script.js
+++ day12/BankAccount/script.js
@@ -1,1 +1,31 @@
-alert("Hello")
+let myBalance = 1500;
+document.getElementById("balance").innerHTML = myBalance;
+
+function makeDeposit() {
+  const amount = parseFloat(document.getElementById("depositAmount").value);
+  myBalance += amount;
+  document.getElementById("balance").innerHTML = myBalance;
+
+  addTransaction("Credit", amount);
+
+}
+
+function makeWithdrawal() {
+  const amount = parseFloat(document.getElementById("withdrawAmount").value);
+  myBalance -= amount;
+  document.getElementById("balance").innerHTML = myBalance;
+  addTransaction("Debit", amount);
+}
+
+function addTransaction(type, amount)
+{
+    const transactionList = document.getElementById("transaction");
+    const transactionItem = document.createElement("li");
+    //transactionItem.textContent = type + ": $ " +amount + " | Balance: $" +myBalance;  
+    //With String Interpolation 
+    transactionItem.textContent = `${type} : $ ${amount} | Balance: $ ${myBalance}`;
+
+    transactionList.appendChild(transactionItem);
+
+
+}
