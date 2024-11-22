58836ad40503a9235852b4e3cbf87d93abd43168
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-11-22 11:29:57 -0500

    .

--- day25/movie-app/script.js
+++ day25/movie-app/script.js
@@ -1,1 +1,7 @@
-alert("Hello")
+value = "girl";
+
+fetch(`https://api.tvmaze.com/singlesearch/shows?q=${value}&embed=episodes`)
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+})
